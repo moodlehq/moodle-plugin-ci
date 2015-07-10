b311d1edf618e2859c6509c2d9738df0685fe6bb
Mark Nielsen <mark@moodlerooms.com>
2015-07-10 15:28:55 -0700

    Attempting to test our tester...

---
+++ tests/fixture/moodle-local_travis/README.md
@@ -0,0 +1,3 @@
+This is a plugin to help test Travis CI build and test process.
+
+Note: the behat.feature file is a copy of Moodle's auth/tests/behat/login.feature
---
+++ tests/fixture/moodle-local_travis/tests/phpunit_test.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Simple test to see if PHPUnit is running
+ *
+ * @package   local_travis
+ * @copyright Copyright (c) 2015 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The test
+ *
+ * @package   local_travis
+ * @copyright Copyright (c) 2015 Moodlerooms Inc. (http://www.moodlerooms.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class local_travis_phpunit_test extends basic_testcase {
+    public function phpunit_test() {
+        // Ensure the sky is not falling.
+        $this->assertTrue(true);
+    }
+}
