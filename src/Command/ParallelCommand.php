a2b5fe28e9f8f04d2ca3cc20c86c0cfe40d07814
Mark Nielsen <mark@moodlerooms.com>
2017-03-30 09:46:51 -0700

    Update license header

--- src/Command/ParallelCommand.php
+++ src/Command/ParallelCommand.php
@@ -6,8 +6,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @copyright Copyright (c) 2015 Moodlerooms Inc. (http://www.moodlerooms.com)
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * Copyright (c) 2017 Blackboard Inc. (http://www.blackboard.com)
+ * License http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 namespace Moodlerooms\MoodlePluginCI\Command;
