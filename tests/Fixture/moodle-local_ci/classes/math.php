3abe72fe04555c5c8b3e31e53cdad50987f045f5
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2022-01-12 12:39:41 +0100

    Bump to codechecker v3.0.5
    Amending the tests to the new Sniffs available. Plus adding a note
    to the ExecuteTest.php because it generates output to php logs and
    always confuses me, looking why it's happening.

--- tests/Fixture/moodle-local_ci/classes/math.php
+++ tests/Fixture/moodle-local_ci/classes/math.php
@@ -24,8 +24,6 @@
 
 namespace local_ci;
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Math class.
  *
--- tests/Fixture/moodle-local_ci/db/upgrade.php
+++ tests/Fixture/moodle-local_ci/db/upgrade.php
@@ -22,8 +22,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Execute local_ci upgrade from the given old version.
  *
